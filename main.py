65a69a9dbd8ca2fba78772076498a40a4e96d3bb
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-07-02 19:17:48 +0200

    modules and imports

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 
-class Car:
+# modulirize python code
+import calc 
+# Or import calc as calculation (alias)
+# Or from calc import * (import all)
+# Or from calc import addtion, subtraction (import specific)
+# Or from calc import addtion as add (import specific with alias)
 
-    # the class constructor is always called __init__
-    # the first argument is always self, which is a reference to the current instance of the class    
-    # self is used to access the members of the class
-    # self is not a keyword, you can use any word you want, but it is a convention to use self
-    # the other arguments are the arguments you pass to the constructor when you create an instance of the class
-    # the constructor is primarily used to initialize the members of the class (a desired state)
-    # __init__ implicitly returns None, returning anything else will raise an error
-
-    
-    def __init__(self, brand, horse_power, color, is_started=False, current_speed=0):
-        self.brand = brand
-        self.horse_power = horse_power
-        self.color = color
-        self.is_started = is_started
-        self.current_speed = current_speed
-
-    
-# Naming conventions in python programming language
-# Class names are written in Pascal, e.g. Car, CarEngine
-# Function names are written in snake_case, e.g. get_car, get_car_engine
-# Variable names are written in snake_case, e.g. car, car_engine
-# Constants are written in CAPITAL_SNAKE_CASE, e.g. MAX_SPEED, MAX_POWER
+print(calc.addtion(1, 2))
+print(calc.subtraction(1, 2))
+print(calc.multiplication(1, 2))
+print(calc.division(1, 2))
+print(calc.PI)
