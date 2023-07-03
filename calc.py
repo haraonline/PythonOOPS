9f5d0a9930a185c7621237b30b3f63f820a06124
haraonline <haragopal.kovvali@gmail.com>
2023-07-03 16:46:27 +0200

    name and main

--- calc.py
+++ calc.py
@@ -1,4 +1,3 @@
-
 PI = 3.14
 
 def addtion(a, b):
@@ -14,3 +13,12 @@ def division(a, b):
     if b == 0:
         raise ValueError("Can't divide by zero!")
     return a / b
+
+
+
+if __name__ == "__main__":
+    print("this part of code is executed only if the calc.py module is run as main program and not when called from other python files")
+
+
+print(__file__) # prints the path of the file
+
--- main.py
+++ main.py
@@ -1,50 +1,16 @@
 
-# # modulirize python code
-# import calc 
-# # Or import calc as calculation (alias)
-# # Or from calc import * (import all)
-# # Or from calc import addtion, subtraction (import specific)
-# # Or from calc import addtion as add (import specific with alias)
-
-# print(calc.addtion(1, 2))
-# print(calc.subtraction(1, 2))
-# print(calc.multiplication(1, 2))
-# print(calc.division(1, 2))
-# print(calc.PI)
-
-
-import math, random
-
-# print(math.pi)
-
-# # what is factorial
-# # 5! = 5 * 4 * 3 * 2 * 1
-# print(math.factorial(5))
-
+import calc, random, math
 
 # random module: EXPLORE LATER
 
-print(random.random())
-print(random.random() * 10)
+list = [1, 2, 3, 4, 5]
+print(list)
 
-print(random.randint(1, 10))
-
-print(random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-print(random.choice(range(1, 10)))
-
-print(random.sample(range(1, 10), 3))
-print(random.sample([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
-print(random.sample(range(1, 10), 3))
-
-print(random.seed(1))
-print(random.uniform(1, 10))
-
-lottery = ["susi", "Carsten", "Dieter", "Hans", "Daniela"]
-random.shuffle(lottery)
-print(random.choice(lottery))
-print(random.choices(lottery, k=2))
-print(random.sample(lottery, k=3)) # ONE VALUE CAN ONLY BE CHOSEN ONCE
+print(calc.addtion(1, 2))
 
 
-
-
+# the keyword pass
+# pass is a null statement in python. Nothing happens when it is executed. It is used as a placeholder.
+# It is used when a statement is required syntactically but you do not want any command or code to execute.
+# The pass statement is a null operation; nothing happens when it executes. 
+# The pass is also useful in places where your code will eventually go, but has not been written yet (e.g., in stubs for example):
